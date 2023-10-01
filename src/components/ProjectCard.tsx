77d6b9c05671a09f50039a7edbbc7f1f61f1bf94
Matias Montagna <matias@platan.us>
2023-10-01 14:20:03 -0300

    feat(projects): add images and redirections to section

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,19 +1,48 @@
-export default function ProjectCard({ title, description, className }:{
+import AppStoreIcon from '@public/icons/app-store-apple-logo.svg';
+import GooglePlayIcon from '@public/icons/google-play-icon.svg';
+import Image from 'next/image';
+
+export default function ProjectCard({
+  title, description, className, imagePath, imageContainerClassNames, playStoreHref, appStoreHref,
+}:{
   title: string
   description: string
   className: string
+  imagePath: string
+  imageContainerClassNames: string
+  playStoreHref: string
+  appStoreHref: string
 }) {
   return (
-    <div className={`flex-col h-80 w-56 rounded-xl shadow ${className}`}>
-      <div className="h-2/5 w-full bg-slate-400 dark:bg-slate-200 rounded-t-xl items-center justify-center">Image Here</div>
-      <div className="flex-col h-3/5 rounded-b-xl w-full dark:bg-neutral-700 justify-between p-4">
+    <div className={`flex-row md:flex-col h-44 md:h-96 w-3/4 md:w-56 rounded-xl shadow ${className}`}>
+      <div className={`h-full w-2/5 md:h-2/5 md:w-full rounded-l-xl md:rounded-tr-xl md:rounded-bl-none items-stretch justify-center ${imageContainerClassNames}`}>
+        <Image
+          src={imagePath}
+          alt={`${title} logo.`}
+          width={80}
+          height={80}
+          style={{
+            width: '100%',
+            objectFit: 'contain',
+          }}
+
+        />
+      </div>
+      <div className="flex-col w-3/5 md:h-3/5 rounded-r-xl md:rounded-bl-xl md:rounded-tr-none md:w-full dark:bg-neutral-700  px-4 pt-4 pb-2 justify-between">
         <div className=" flex-col items-center space-y-2">
           <p className="dark:text-stone-300 font-medium">{title}</p>
           <p className="text-stone-500 dark:text-stone-300 font-light">{description}</p>
         </div>
-        <div className="flex-row justify-between px-4">
-          <p>Preview</p>
-          <p>Github</p>
+        <div className="flex-row w-full justify-end md:justify-between items-center md:px-6">
+          <a
+            href={appStoreHref}
+            title="Download on the App Store"
+          >
+            <AppStoreIcon className="text-5xl" />
+          </a>
+          <a href={playStoreHref} title="Get it on Google Play">
+            <GooglePlayIcon className="text-5xl" />
+          </a>
         </div>
       </div>
     </div>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { SUBTITLE_CLASSNAMES, TITLE_CLASSNAMES } from '@/utils/constants';
 import ProjectCard from './ProjectCard';
 
@@ -5,26 +7,50 @@ const PROJECTS_DATA = [
   {
     title: 'Duit',
     description: 'Mobile app where users execute routines of tasks made by their bosses.',
+    imagePath: '/images/duit-icon.png',
+    imageContainerClassNames: 'bg-blue-700',
+    appStoreHref: 'https://apps.apple.com/app/duit-app/id1279375659',
+    playStoreHref: 'https://play.google.com/store/apps/details?id=cl.arenalabs.duitapp&hl=en&gl=US&pli=1',
   },
   {
     title: 'Cleary',
     description: 'Mobile app for the Cleary Web App',
+    imagePath: '/images/cleary-icon.png',
+    imageContainerClassNames: 'bg-white border-r border-r-slate-200 md:border-r-none md:border-b md:border-b-slate-200 dark:border-0',
+    appStoreHref: 'https://apps.apple.com/by/app/cleary/id6449472335',
+    playStoreHref: 'https://play.google.com/store/apps/details?id=com.gocleary.cleary_mobile_app&hl=en_US',
   },
   {
     title: 'Re App',
     description: 'Mobile App that enhances recycling by listing nearby recyling stations and more.',
+    imagePath: '/images/re-app-icon.png',
+    imageContainerClassNames: 'bg-blue-500',
+    appStoreHref: 'https://apps.apple.com/us/app/re-econom%C3%ADa-circular/id1529547693',
+    playStoreHref: 'https://play.google.com/store/apps/details?id=com.Triciclos.ReApp&hl=es_CL',
   },
 ];
 
 export default function Projects() {
   return (
-    <div className="flex-col space-y-10">
+    <div className="flex-col space-y-10 w-screen items-center">
       <div className="flex-col justify-between items-center  h-20 md:h-24">
         <p className={TITLE_CLASSNAMES}>Projects</p>
         <p className={SUBTITLE_CLASSNAMES}>Things I&apos;ve worked on so far</p>
       </div>
-      <div className="flex-row flex-wrap">
-        {PROJECTS_DATA.map(({ title, description }) => <ProjectCard title={title} description={description} className="m-4" />)}
+      <div className="flex-col md:flex-row flex-wrap items-center">
+        {PROJECTS_DATA.map(({
+          title, description, imagePath, imageContainerClassNames, appStoreHref, playStoreHref,
+        }) => (
+          <ProjectCard
+            title={title}
+            description={description}
+            imagePath={imagePath}
+            imageContainerClassNames={imageContainerClassNames}
+            appStoreHref={appStoreHref}
+            playStoreHref={playStoreHref}
+            className="m-2 md:m-4"
+          />
+        ))}
       </div>
     </div>
   );
