599c2e59c52da36f7f2488c6ae297814e56ecea6
Matias Montagna <matias@platan.us>
2023-10-14 00:14:52 -0300

    feat(nav): mobile side navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import Image from 'next/image';
 
 import BurgerMenuIcon from './BurgerMenuIcon';
@@ -12,11 +16,13 @@ const NAV_OPTIONS = [
 
 const NAV_SOCIALS = [
   {
+    title: 'Github',
     logo: '/icons/github-icon.svg',
     url: 'https://github.com/MatiasMontagna',
     description: 'The Github logo.',
   },
   {
+    title: 'Linkedin',
     logo: '/icons/linkedin-icon.svg',
     url: 'https://www.linkedin.com/in/mat%C3%ADas-montagna-38309b1b6/',
     description: 'The Linkedin logo.',
@@ -24,41 +30,49 @@ const NAV_SOCIALS = [
 ];
 
 export default function Nav() {
+  const [isNavVisible, setisNavVisible] = useState(false);
+  const toggleNav = () => setisNavVisible(!isNavVisible);
+
   return (
-    <nav className="flex flex-row justify-between w-screen md:w-full mt-8">
+    <nav className="flex relative flex-row bg-white dark:bg-black justify-between w-screen md:w-full mt-8">
       <div className="flex items-center justify-center w-1/4 text-stone-500 dark:text-neutral-400">
         MMC
       </div>
-      <div className="flex justify-end pr-12 md:hidden w-3/4">
+      <div className="flex justify-end pr-8 md:hidden w-3/4">
         <button
+          onClick={toggleNav}
           type="button"
         >
           <BurgerMenuIcon />
         </button>
       </div>
-      <div className="hidden md:flex flex-row w-3/4 justify-center items-center space-x-16 pl-10">
-        <ul className="flex flex-row justify-center space-x-10">
+      <div className={`${isNavVisible ? 'flex' : 'hidden'} md:flex transition-all md:transition-none duration-700 ease-in bg-inherit flex-column md:flex-row md:w-3/4 justify-center items-center md:space-x-16 md:pl-10`}>
+        <ul className="flex top-9 right-0 h-[80vh]  md:h-auto  absolute md:static w-1/2 md:w-auto bg-inherit flex-col md:flex-row md:justify-center space-y-10 md:space-x-10 md:space-y-0">
           { NAV_OPTIONS.map((option) => (
             <li
               key={option}
-              className="font-medium text-lg text-stone-500 dark:text-neutral-400"
+              className="font-medium text-lg text-stone-500 dark:text-neutral-400 pl-7 md:pl-0"
             >
               {option}
             </li>
           ))}
-        </ul>
-        <div className="flex flex-row space-x-6">
           {NAV_SOCIALS.map((social) => (
-            <a href={social.url}>
-              <Image
-                src={social.logo}
-                alt={social.description}
-                width={30}
-                height={30}
-              />
-            </a>
+            <li>
+              <a
+                className="flex pl-6 space-x-6 items-end"
+                href={social.url}
+              >
+                <Image
+                  src={social.logo}
+                  alt={social.description}
+                  width={30}
+                  height={30}
+                />
+                <p className="font-medium text-lg text-stone-500 dark:text-neutral-400 md:hidden">{social.title}</p>
+              </a>
+            </li>
           ))}
-        </div>
+        </ul>
       </div>
     </nav>
   );
